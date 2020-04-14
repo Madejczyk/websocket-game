1b58a10d634a459ea85ad21c956ba2f1f9205614
Madejczyk <jakub.madejczyk92@gmail.com>
2020-04-14 15:52:59 +0200

    TEST

--- packages/backend/src/sequelize.ts
+++ packages/backend/src/sequelize.ts
@@ -4,7 +4,11 @@ import { config } from './config/config'
 const c =
   process.env.NODE_ENV === 'test' ? config.postgressTest : config.postgress
 
-export const sequelize = new Sequelize({
+export const sequelize = process.env.NODE_ENV === 'test' ? new Sequelize(
+  process.env.DB_TEST_URI || '', {
+  dialect: 'postgres',
+  storage: ':memory:'
+}) : new Sequelize({
   database: c.database,
   dialect: 'postgres',
   username: c.username,
