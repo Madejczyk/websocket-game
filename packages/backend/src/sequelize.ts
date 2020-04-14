672fe7a9521bad53202461d742682c90b76833eb
Madejczyk <jakub.madejczyk92@gmail.com>
2020-04-14 15:58:03 +0200

    Revert "TEST"
    This reverts commit 1b58a10d634a459ea85ad21c956ba2f1f9205614.

--- packages/backend/src/sequelize.ts
+++ packages/backend/src/sequelize.ts
@@ -4,11 +4,7 @@ import { config } from './config/config'
 const c =
   process.env.NODE_ENV === 'test' ? config.postgressTest : config.postgress
 
-export const sequelize = process.env.NODE_ENV === 'test' ? new Sequelize(
-  process.env.DB_TEST_URI || '', {
-  dialect: 'postgres',
-  storage: ':memory:'
-}) : new Sequelize({
+export const sequelize = new Sequelize({
   database: c.database,
   dialect: 'postgres',
   username: c.username,
