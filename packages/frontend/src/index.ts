b45bb318477d3af2014156a4bd3b37735d7155c2
Jakub Madejczyk <Madejczyk@users.noreply.github.com>
2020-04-17 21:13:31 +0200

    Update packages/frontend/src/index.ts

--- packages/frontend/src/index.ts
+++ packages/frontend/src/index.ts
@@ -8,7 +8,6 @@ if ('serviceWorker' in navigator) {
         )
       },
       function (err) {
-        // registration failed :(
         console.log('ServiceWorker registration failed: ', err)
       }
     )
